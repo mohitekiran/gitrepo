c738bd2cea9c310c8a88b91e87c2e0986fc24c37
kmohite <kmohite@Kiran>
2018-09-15 17:30:40 +0530

    Initial checkin

---
+++ SeleniumMavenFlow/src/test/java/searchGoogle.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class searchGoogle {
+
+	static WebDriver driver;
+
+	@BeforeTest
+	public void openBrowser() {
+		System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\libs\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void searchAndClick() {
+		driver.get("https://www.google.com/");
+		driver.manage().window().fullscreen();
+
+		WebElement googleSearchBox = driver.findElement(By.id("lst-ib"));
+		googleSearchBox.sendKeys("Wikipedia");
+		googleSearchBox.sendKeys(Keys.ENTER);
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
+}
